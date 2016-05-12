15c2e5eca6e5ed8993583fd446329f3a3062118f
Vasilyuk Vasiliy <vasilyuk.vasiliy@gmail.com>
2016-05-12 19:03:08 +0300

    Update to v2.0; Add License MIT, Copyright (c) 2016 Vasilyuk Vasiliy.

--- install-megatools.sh
+++ install-megatools.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
-sudo apt-get update;
+#!/bin/sh
+# info: megatools autoinstall script
+# options: VERSION
+# version: v1.1
+#
+# Copyright (c) 2016 Vasilyuk Vasiliy <vasilyuk.vasiliy@gmail.com>
+
+
+sudo apt-get update
 sudo apt-get --reinstall -y install build-essential libglib2.0-dev libssl-dev libcurl4-openssl-dev libgirepository1.0-dev g++
 if [ -z $1 ]; then
 	VERSION="1.9.97";
 else
-	VERSION=$1;
+	VERSION=${1};
 fi;
 wget http://megatools.megous.com/builds/megatools-$VERSION.tar.gz
 zcat megatools-$VERSION.tar.gz > megatools-$VERSION.tar
