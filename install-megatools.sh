c8e0ec8fde46ddbf37f7b5f4fee89c832651ecfa
Vasilyuk Vasiliy <vasilyuk.vasiliy@gmail.com>
2016-11-29 16:29:19 +0300

    Update files
    Fix version
    Drop analitics
    Fix links
    New format readme

--- install-megatools.sh
+++ install-megatools.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # info: megatools autoinstall script
 # options: VERSION
-# version: v1.1
+# version: v1.1.3
 #
 # Copyright (c) 2016 Vasilyuk Vasiliy <vasilyuk.vasiliy@gmail.com>
 
@@ -9,13 +9,13 @@
 sudo apt-get update
 sudo apt-get --reinstall -y install build-essential libglib2.0-dev libssl-dev libcurl4-openssl-dev libgirepository1.0-dev g++
 if [ -z $1 ]; then
-	VERSION="1.9.97";
+  VERSION="1.9.97";
 else
 	VERSION=${1};
 fi;
 wget http://megatools.megous.com/builds/megatools-$VERSION.tar.gz
 zcat megatools-$VERSION.tar.gz > megatools-$VERSION.tar
-tar -xf megatools-$VERSION.tar   
+tar -xf megatools-$VERSION.tar
 cd megatools-$VERSION
 ./configure
 make
